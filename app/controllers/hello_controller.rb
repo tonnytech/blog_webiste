25fabaff3dc572d15101926e64709847292536d3
tonnytech <tonnytei4@gmail.com>
2023-09-22 15:53:10 +0300

    remove linter errors

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # The HelloController handles requests related to greetings.
 # It provides endpoints for saying hello to users.
 class HelloController < ApplicationController
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # The ApplicationMailer class is the base class for all mailers in the application.
 # It provides common functionality and configuration for sending emails.
 class ApplicationMailer < ActionMailer::Base
