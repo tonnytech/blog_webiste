d6d1bbf91d6172d4f59d3b12ab11114aebba76cb
tonnytech <tonnytei4@gmail.com>
2023-09-26 12:48:56 +0300

    remove liter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
-class PostsController < ApplicationController # rubocop:todo Style/Documentation
-  def index; end
+class PostsController < ApplicationController
+  
+  def index
+  end
 
   def show
-      puts params # rubocop:todo Layout/IndentationWidth
+      puts params
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
-class UsersController < ApplicationController # rubocop:todo Style/Documentation
+class UsersController < ApplicationController
   def index; end
 
   def show
-      puts params # rubocop:todo Layout/IndentationWidth
+      puts params
   end
 end
