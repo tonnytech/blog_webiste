ecc45d1e00d8dc939f7177d80a066549c15a6bbd
tonnytech <tonnytei4@gmail.com>
2023-09-29 16:49:44 +0300

    add views template for the project

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Comment, type: :model do
     @post = Post.create(title: 'Posttitle', text: 'PostText', author_id: @user.id)
   end
 
-  it 'initial count to be nil' do
-    expect(@post.comments_counter).to eq(nil)
+  it 'initial count to be 0' do
+    expect(@post.comments_counter).to eq(0)
   end
 end
--- spec/like_spec.rb
+++ spec/like_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Like, type: :model do
     @post = Post.create(title: 'postTitle', text: 'postText', author_id: @user.id)
   end
 
-  it 'initial like counter to be nil' do
-    expect(@post.likes_counter).to eq(nil)
+  it 'initial like counter to be 0' do
+    expect(@post.likes_counter).to eq(0)
   end
 end
