484ccd1fad808464b1cf544d09bacd4454d2a6ef
BenM4k <bennymakomo@gmail.com>
2023-10-07 08:04:22 +0200

    Set current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # def current_user
   #   User.find(1)
   # end
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+      User.find_by(id: session[:current_user_id])
+  end
 end
